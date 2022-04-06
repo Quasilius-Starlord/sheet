76c5a45d50b87505ad476c9b6784de40d1ca8d15
Quasilius-Starlord <joysen833@gmail.com>
2022-04-06 07:41:02 -0700

    stte

--- Array Subset of another array.cpp
+++ Array Subset of another array.cpp
@@ -5,7 +5,7 @@
 class Solution{
     std::string isSubset(int a1[], int a2[], int n, int m) {
         std::unordered_set<int> set;
-
+        //status
         for (int i = 0; i < n; i++)
             set.insert(a1[i]);
         for (int i = 0; i < m; i++)
